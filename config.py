afc36d5295c35309293b1cfc353fb0336c021b83
Harmon Herring <harmonherring@gmail.com>
2020-05-08 03:58:20 -0500

    Begin flask-pyoidc integration

--- config.py
+++ config.py
@@ -9,3 +9,11 @@ PORT = environ.get('CTF_PORT', 8080)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = environ.get('CTF_DATABASE_URI', 'sqlite:////{}'.format(path.join(getcwd(), 'data.db')))
+
+# OpenID Connect SSO config
+OIDC_ISSUER = environ.get('CTF_OIDC_ISSUER', 'https://sso.csh.rit.edu/auth/realms/csh')
+OIDC_CLIENT_CONFIG = {
+    'client_id': environ.get('CTF_OIDC_CLIENT_ID', 'ctf'),
+    'client_secret': environ.get('CTF_OIDC_CLIENT_SECRET', ''),
+    'post_logout_redirect_uris': [environ.get('CTF_OIDC_LOGOUT_REDIRECT_URI', 'https://quotefault-api.csh.rit.edu/logout')]
+}
--- ctf/__init__.py
+++ ctf/__init__.py
@@ -6,12 +6,16 @@ Loads blueprints to their respective routes.
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_pyoidc.flask_pyoidc import OIDCAuthentication
 
 import config
 
 app = Flask(__name__)
 app.config.from_object(config)
-
+auth = OIDCAuthentication(
+    app,
+    issuer=app.config['OIDC_ISSUER'],
+    client_registration_info=app.config['OIDC_CLIENT_CONFIG'])
 db = SQLAlchemy(app)
 
 # pylint: disable=wrong-import-position
