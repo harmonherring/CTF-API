c1c0e01ff74361e84954c714589dae8499b5a233
Harmon Herring <harmonherring@gmail.com>
2020-05-09 17:30:52 -0500

    Remove the dumb checks I wrote at 4am

--- ctf/utils.py
+++ ctf/utils.py
@@ -10,68 +10,6 @@ from flask_sqlalchemy import Model
 from ctf.ldap import is_ctf_admin
 
 
-def run_checks(**kwargs) -> (dict, int):
-    """
-    Runs the checks specified, return errors if needed, return None if all checks pass
-    Most of these checks will fail if not run inside of a Flask route.
-    """
-    keys = kwargs.keys()
-    print(keys)
-    print("IS IT HERE: " + str("ensure_existence" in keys))
-    if "is_authorized" in keys:
-        # Check to see if current user is admin or is the user specified by associated kwarg
-        current_user = session['userinfo'].get('preferred_username')
-        if not current_user:
-            return {
-                'status': "error",
-                'message': "Your session doesn't have the 'preferred_username' value"
-            }, 401
-        if not (is_ctf_admin(current_user) or current_user == kwargs['is_authorized']):
-            return {
-                'status': "error",
-                'message': "You aren't authorized for this action"
-            }, 403
-    if "has_json_args" in keys:
-        if not request.is_json:
-            return {
-                'status': "error",
-                'message':
-                    kwargs['is_json'] if kwargs.get('is_json') else
-                    "Content-Type must be application/json"
-            }, 415
-        if kwargs['has_json_args']:
-            data = request.get_json()
-            missing_args = []
-            for json_arg in kwargs['has_json_args']:
-                if not data.get(json_arg):
-                    missing_args.append(json_arg)
-            if len(missing_args) > 0:
-                return {
-                    'status': "error",
-                    'message': "You're missing at least one of the required arguments in your "
-                               "application/json body: " + ', '.join([str(missing_arg) for
-                                                                      missing_arg in missing_args])
-                }, 422
-    if "ensure_existence" in keys:
-        print("Ensuring existence")
-        # This test ensures that the objects passed into the associated kwargs list exist.
-        for argument in kwargs['ensure_existence']:
-            print(argument)
-            if not argument:
-                return {
-                    'status': "error",
-                    'message': argument.__class__.__name__ + " does not exist"
-                }, 404
-    if "ensure_nonexistence" in keys:
-        # This test ensures that the obejcts passed into the associated kwargs list don't exist
-        for argument in kwargs['ensure_nonexistence']:
-            if argument:
-                return {
-                    'status': "error",
-                    'message': argument.__clas__.__name__ + " already exists"
-                }, 409
-
-
 class TSAPreCheck:
     """
     Contains the functions for performing various checks. Most of these checks require access to
