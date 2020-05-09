2b8159eef2b7fe05181b54a8270ee5eeca2f3968
Harmon Herring <harmonherring@gmail.com>
2020-05-09 17:18:38 -0500

    Integrate new precheck into difficulty routes

--- ctf/routes/difficulties.py
+++ ctf/routes/difficulties.py
@@ -7,7 +7,7 @@ from flask import Blueprint, request, jsonify
 
 from ctf import auth
 from ctf.models import Difficulty
-from ctf.utils import run_checks
+from ctf.utils import TSAPreCheck
 
 difficulties_bp = Blueprint("difficulties", __name__)
 
@@ -25,15 +25,14 @@ def all_difficulties():
         return jsonify([difficulty.to_dict()['name'] for difficulty in Difficulty.query.all()]),\
                200
     elif request.method == 'POST':
-        checker = run_checks(is_authorized=None, has_json_args=["name"])
-        if checker is not None:
-            return jsonify(checker[0]), checker[1]
         data = request.get_json()
-        if Difficulty.query.filter_by(name=data['name']).first():
-            return jsonify({
-                'status': "error",
-                'message': "Difficulty already exists"
-            }), 409
+        check_difficulty = Difficulty.query.filter_by(name=data.get('name') if data.get('name')
+                                                      else None)
+        precheck = TSAPreCheck().is_authorized(None).has_json_args("name").ensure_nonexistence((
+            check_difficulty, Difficulty))
+        if precheck.error_code:
+            return jsonify(precheck.message), precheck.error_code
+
         new_difficulty = Difficulty.create(data['name'])
         return jsonify(new_difficulty), 201
 
@@ -47,14 +46,9 @@ def single_difficulty(difficulty_name: str):
     :DELETE: Deletes the Difficulty identified by difficulty_name
     """
     if request.method == 'DELETE':
-        checker = run_checks(is_authorized=None)
-        if checker is not None:
-            return jsonify(checker[0]), checker[1]
         difficulty = Difficulty.query.filter_by(name=difficulty_name).first()
-        if not difficulty:
-            return jsonify({
-                'status': "error",
-                'message': "Difficulty doesn't exist"
-            }), 404
+        precheck = TSAPreCheck().is_authorized(None).ensure_existence((difficulty, Difficulty))
+        if precheck.error_code:
+            return jsonify(precheck.message), precheck.error_code
         difficulty.delete()
         return '', 204
