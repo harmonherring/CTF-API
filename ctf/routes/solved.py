a36fb144daf45fbe143e5b386ab9a8dd78f691f0
Harmon Herring <harmonherring@gmail.com>
2020-05-09 17:38:34 -0500

    Mount solved routes at a different location

--- ctf/routes/solved.py
+++ ctf/routes/solved.py
@@ -6,22 +6,23 @@ These API routes are poorly named. They keep a record of who has solved which fl
 from flask import Blueprint, jsonify, request
 
 from ctf import auth
-from ctf.models import Solved, Flag
+from ctf.models import Solved, Flag, Challenge
 from ctf.utils import TSAPreCheck
 
 solved_bp = Blueprint('solved', __name__)
 
 
-@solved_bp.route('/<int:flag_id>', methods=['GET'])
+@solved_bp.route('/<int:challenge_id>/flags/<int:flag_id>/solved', methods=['GET'])
 @auth.oidc_auth
-def solved_flags(flag_id: int):
+def solved_flags(challenge_id: int, flag_id: int):
     """
     Operations pertaining to the solution of flags
 
     :GET: Get a list of the users who have solved this flag
     """
     flag = Flag.query.filter_by(id=flag_id).first()
-    precheck = TSAPreCheck().ensure_existence((flag, Flag))
+    challenge = Challenge.query.filter_by(id=challenge_id).first()
+    precheck = TSAPreCheck().ensure_existence((challenge, Challenge), (flag, Flag))
     if precheck.error_code:
         return jsonify(precheck.message), precheck.error_code
 
